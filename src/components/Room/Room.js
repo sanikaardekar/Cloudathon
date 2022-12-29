7ff27820ca7cc01814cbdeb99fc2cf43a82b0c6f
Jay Jain <jay.jain@neyx.in>
2022-12-29 20:26:24 +0530

    nginx configuration added and room link updated

--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -4,10 +4,14 @@ import "./Room.css";
 const Room = () => {
   return (
     <div className="whereby">
-      <iframe
+      {/* <iframe
         src="https://coders-4-life.whereby.com/elite-coders95b53a50-541b-4fb9-a98e-641b996fe65d"
         allow="camera; microphone; fullscreen; speaker; display-capture"
         title="Whereby"
+      ></iframe> */}
+      <iframe
+        src="https://coders-4-life.whereby.com/demo-b9fb3441-cf4a-45b0-a3f2-b2870a269d0d"
+        allow="camera; microphone; fullscreen; speaker; display-capture"
       ></iframe>
     </div>
   );
